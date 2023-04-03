510780111cb10d69d81d1b4daafed8f0d81c46e6
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-04-03 17:21:31 +0900

    links js to html and test showing content

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello js!");
+const word = document.querySelector(".showText");
+const lives = document.querySelector(".showLives");
+// console.log(word.textContent + lives.textContent);
+for(let a of word.textContent) {
+    console.log(a);
+}
