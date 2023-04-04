190fa965e30e5c33a56085d11abe36c71dd95982
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-04-04 22:58:46 +0900

    기초 실력 다지기
    이벤트 리스너 등을 이용하여 리스트에 동적으로 요소 생성함

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const word = document.querySelector(".showText");
+const word = document.querySelector("#word");
 const lives = document.querySelector(".showLives");
+const btn = document.querySelector("#btn");
+const form = document.querySelector("#submit");
+const list = document.querySelector(".list");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const newLi = document.createElement('li');
+    newLi.innerText = word.value;
+    list.appendChild(newLi);
+    console.log(word.value);
+
+})
 // console.log(word.textContent + lives.textContent);
-for(let a of word.textContent) {
-    console.log(a);
-}
+btn.addEventListener("click", ()=>{
+    
+})
+lives.innerHTML = "<h1>hi</h1>";
+// console.log(lives);
