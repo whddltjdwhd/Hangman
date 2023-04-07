f9f87396500c8029ef9cb88f826c4c44f70640ef
Rune <81473218+whddltjdwhd@users.noreply.github.com>
2023-04-07 22:00:52 +0900

    행맨 기초작업 1
    글자 저장 및 정답 확인 기능구현

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
-const word = document.querySelector("#word");
+const words = document.querySelector("#words");
 const lives = document.querySelector(".showLives");
-const btn = document.querySelector("#btn");
-const form = document.querySelector("#submit");
-const list = document.querySelector(".list");
 
-form.addEventListener("submit", (e)=>{
+const generator = document.querySelector("#generate");
+const alp = document.querySelector(".alp");
+
+const checkAnswer = document.querySelector(".answer_container");
+const answer = document.querySelector("#answer");
+
+let saveWord = "";
+
+generator.addEventListener("submit", (e)=>{
     e.preventDefault();
-    const newLi = document.createElement('li');
-    newLi.innerText = word.value;
-    list.appendChild(newLi);
-    console.log(word.value);
+    if(alp.children.length > 0) {
+        const childLen = alp.children.length;
+        for(let i = 0; i < childLen; i++) {
+            const lis = document.querySelector("li");
+            alp.removeChild(lis);
+        }
+    }
 
+    let tmpWord = words.value;
+
+    for(let a of tmpWord) {
+        const newLi = document.createElement('li');
+        newLi.innerText = a;
+        alp.appendChild(newLi);
+    }
+    // wordArr.push(words.value);
+    saveWord = words.value;
+    words.value = "";
 })
-// console.log(word.textContent + lives.textContent);
-btn.addEventListener("click", ()=>{
-    
+
+checkAnswer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(saveWord == "") alert("정답이 아직 없소..");
+    else {
+        if(saveWord == answer.value) {
+            console.log("정답!");
+        } else {
+            console.log("틀렸소")
+        }
+    }
 })
-lives.innerHTML = "<h1>hi</h1>";
-// console.log(lives);
